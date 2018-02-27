9021dfaf51df456ec0a39add6d0fd6c78ad64bd3
Kofoworola Olabisi <toyosio@gmail.com>
2018-02-27 04:16:59 -0600

    complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,66 +5,17 @@ var friendList = [
     name: 'Toyosi Olabisi',
     profilePic: 'https://avatars3.githubusercontent.com/u/31281599?s=460&v=4',
     scores: [
-      "4",
       "5",
+      "3",
       "5",
       "2",
+      "3",
+      "2",
       "4",
-      "5",
-      "4",
-      "4",
+      "1",
       "3",
       "4"
     ]
-  },
-  {
-  name: 'Jadon Dismuke',
-  profilePic: 'https://avatars3.githubusercontent.com/u/31281599?s=460&v=4',
-  scores: [
-    "4",
-    "5",
-    "5",
-    "2",
-    "4",
-    "5",
-    "4",
-    "4",
-    "3",
-    "4"
-  ]
-},
-{
-name: 'Lolade Olabisi',
-profilePic: 'https://avatars3.githubusercontent.com/u/31281599?s=460&v=4',
-scores: [
-  "4",
-  "5",
-  "5",
-  "2",
-  "4",
-  "5",
-  "4",
-  "4",
-  "3",
-  "4"
-]
-},
-{
-name: 'M Jeffries',
-profilePic: 'https://avatars3.githubusercontent.com/u/31281599?s=460&v=4',
-scores: [
-  "4",
-  "5",
-  "5",
-  "2",
-  "4",
-  "5",
-  "4",
-  "4",
-  "3",
-  "4"
-]
-}
-
+  }
 }
 module.exports = friendList;
