047edbe723321c13e608e473fddf1ad1831bf39b
Kofoworola Olabisi <toyosio@gmail.com>
2018-02-27 06:06:46 -0600

    complete

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,44 +1,45 @@
-//npm
+//npm and routes
 var friendList = require('../data/friends.js');
 var path = require('path');
 
-//route
-app.get('/api/friends', function (req, res) {
-	res.json(friends);
-});
+module.exports = function (app) {
+	//a GET route that displays JSON of all possible friends
+	app.get('/api/friends', function (req, res) {
+		res.json(friendList);
+	});
 
-//push, compares friends, loops through friends list and scores
-app.post('/api/friends', function (req, res) {
+	//push, compares friends, loops through friends list and scores
+	app.post('/api/friends', function (req, res) {
 
-	var newFriendScores = req.body.scores;
-	var scoresArray = [];
-	var friendCount = 0;
-	var bestMatch = 0;
+		var newFriendScores = req.body.scores;
+		var scoresArray = [];
+		var friendCount = 0;
+		var bestMatch = 0;
 
 
-	for (var i = 0; i < [friendList].length; i++) {
-		var scoresDiff = 0;
+		for (var i = 0; i < [friendList].length; i++) {
+			var scoresDiff = 0;
 
-		for (var j = 0; j < newFriendScores.length; j++) {
-			scoresDiff += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
+			for (var j = 0; j < newFriendScores.length; j++) {
+				scoresDiff += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
+			}
+
+
+			scoresArray.push(scoresDiff);
 		}
 
-
-		scoresArray.push(scoresDiff);
-	}
-
-	//find bestie match
-	for (var i = 0; i < scoresArray.length; i++) {
-		if (scoresArray[i] <= scoresArray[bestMatch]) {
-			bestMatch = i;
+		//find bestie match
+		for (var i = 0; i < scoresArray.length; i++) {
+			if (scoresArray[i] <= scoresArray[bestMatch]) {
+				bestMatch = i;
+			}
 		}
-	}
 
-	//return bestMatch
-	var bff = friendList[bestMatch];
-	res.json(bff);
+		//return bestMatch
+		var bff = friendList[bestMatch];
+		res.json(bff);
 
-	//pushe new submission
-	friendList.push(req.body);
-});
-};
+		//pushe new submission
+		friendList.push(req.body);
+	});
+}
