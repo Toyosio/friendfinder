38876dd2a67327ed33bcc2aeacffb04e048bade9
Kofoworola Olabisi <toyosio@gmail.com>
2018-02-27 03:42:46 -0600

    complete

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Toyosi Olabisi',
+    profilePic: 'https://avatars3.githubusercontent.com/u/31281599?s=460&v=4',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },
+  {
+  name: 'Jadon Dismuke',
+  profilePic: 'https://avatars3.githubusercontent.com/u/31281599?s=460&v=4',
+  scores: [
+    "4",
+    "5",
+    "5",
+    "2",
+    "4",
+    "5",
+    "4",
+    "4",
+    "3",
+    "4"
+  ]
+},
+{
+name: 'Lolade Olabisi',
+profilePic: 'https://avatars3.githubusercontent.com/u/31281599?s=460&v=4',
+scores: [
+  "4",
+  "5",
+  "5",
+  "2",
+  "4",
+  "5",
+  "4",
+  "4",
+  "3",
+  "4"
+]
+},
+{
+name: 'M Jeffries',
+profilePic: 'https://avatars3.githubusercontent.com/u/31281599?s=460&v=4',
+scores: [
+  "4",
+  "5",
+  "5",
+  "2",
+  "4",
+  "5",
+  "4",
+  "4",
+  "3",
+  "4"
+]
+}
+
+}
+module.exports = friendList;
